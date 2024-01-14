e55e9e13ec9c74fc9d2c4d152cfa88580ea330ae
preet <preet.viradiya@mail.utoronto.ca>
2024-01-13 20:21:56 -0500

    First

--- Assignment 1/data_collator.py
+++ Assignment 1/data_collator.py
@@ -10,17 +10,13 @@ def generate_final_data(data_dir="data", save_dir="data.csv"):
         if files:
             for file in files:
                 logging.log(60, "Processing file: {}".format(file))
-                if file.endswith(".csv") and file != "data.csv":
-                    df = pd.read_csv(os.path.join(root, file), index_col='ISIN')
-                    data = df if data is None else pd.concat([data, df], ignore_index=False, axis=0)
+                if file.endswith(".csv") and file not in {'data.csv', 'cleaned_data.csv', 'final_data.csv'}:
+                    df = pd.read_csv(os.path.join(root, file))
+                    data = df if data is None else pd.concat([data, df], ignore_index=True, axis=0)
     logging.log(60, "Finished processing")
     data['Coupon'] = data['Coupon'].str.rstrip('%').astype('float')
-    data['Maturity Date'] = pd.to_datetime(data['Maturity Date'], format='mixed')
-    data['Issue Date'] = pd.to_datetime(data['Issue Date'], format='mixed')
-    data['Bond Days'] = (data['Maturity Date'] - data['Issue Date']).dt.days
-    data['Bond Months'] = data['Bond Days']/30
-    data['Bond Years'] = data['Bond Months']/12
-    data.to_csv(os.path.join(data_dir, save_dir))
+    data['Bond Price'] = (data['Ask'] + data['Bid']) / 2
+    data.to_csv(os.path.join(data_dir, save_dir), index=False)
 
 
 if __name__ == "__main__":
--- Assignment 1/utils.py
+++ Assignment 1/utils.py
@@ -1,3 +1,7 @@
+from collections import Counter
+from datetime import datetime
+import seaborn as sns
+import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
@@ -11,20 +15,76 @@ urls = {
 
 base_url = "https://markets.businessinsider.com"
 
+
 def plot_coupon_issue(df):
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Colormap normalization
-    norm = Normalize(vmin=df['Bond Years'].min(), vmax=df['Bond Years'].max())
+    norm = Normalize(vmin=df['Maturity Year'].min(), vmax=df['Maturity Year'].max())
     sm = ScalarMappable(cmap='viridis', norm=norm)
     sm.set_array([])
 
     scatter = ax.scatter(df['Issue Year'], df['Coupon'], marker='o',
-                         label='Coupon Percent (Issue)', c=df['Bond Years'], cmap='viridis')
+                         label='Coupon Percent (Issue)', c=df['Maturity Year'], cmap='viridis')
 
-    cbar = plt.colorbar(sm, ax=ax, label='Bond Years')
+    cbar = plt.colorbar(sm, ax=ax, label='Maturity Year')
     ax.set_xlabel('Issue Date')
     ax.set_ylabel('Coupon %')
-    ax.set_title('Coupon % vs. Issue Year (Colored by Bond Years)')
+    ax.set_title('Coupon % vs. Issue Year (Colored by Maturity Year)')
     ax.grid(True)
     plt.show()
+
+
+def get_bonds(df):
+    start_date = pd.to_datetime('2024-02-05', format="%Y-%m-%d")
+    end_date = start_date + pd.DateOffset(years=5) + pd.DateOffset(months=6)
+    df = df[(df['Maturity Date'] > start_date) & (df['Maturity Date'] <= end_date)]
+    return df
+
+def generate_range():
+    start_date = pd.to_datetime('2024-02-05', format="%Y-%m-%d")
+    end_date = start_date + pd.DateOffset(years=5)
+
+    date_ranges = pd.date_range(start=start_date, end=end_date, periods=10)
+
+    return date_ranges.tolist()
+
+
+def plot_bonds_price(df):
+    plt.figure(figsize=(30, 10))
+    sns.scatterplot(x='Maturity Date', y='Bond Price', data=df)
+
+    for i, row in df.iterrows():
+        plt.text(row['Maturity Date'], row['Bond Price'], f'{row["ISIN"]}', fontsize=8, ha='right', va='bottom')
+
+    plt.title('Bond Price vs. Maturity Date')
+    plt.xlabel('Maturity Date')
+    plt.ylabel('Price')
+    plt.xticks(rotation=45)
+
+    for target_date in generate_range():
+        plt.axvline(x=target_date, color='red', linestyle='--', label=f'{target_date}')
+
+    plt.legend()
+
+    plt.grid(True)
+    plt.show()
+
+def years_to_maturity(issue_date, maturity_date):
+    issue_date = datetime.strptime(issue_date, '%m/%d/%Y')
+    maturity_date = datetime.strptime(maturity_date, '%m/%d/%Y')
+
+    time_to_maturity = int((maturity_date - issue_date).days / 365.2425)
+    return time_to_maturity
+
+
+def calculate_num_compounding_periods(coupon_rate, num_years):
+    return num_years * coupon_rate
+
+
+def calculate_semi_annual_coupon_rate(coupon_rate):
+    return coupon_rate / 2
+
+
+def calculate_semi_annual_coupon(coupon_rate, face_value):
+    return coupon_rate * face_value
